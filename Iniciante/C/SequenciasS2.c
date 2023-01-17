20ef70d9d753b9dafa5349b131c7dfc8766dc4c4
yodemisj <yodemisj@gmail.com>
2023-01-17 20:06:29 -0300

    Resolução do exercício SequenciasSII

--- Iniciante/C/SequenciasS2.c
+++ Iniciante/C/SequenciasS2.c
@@ -5,11 +5,14 @@ int main() {
     float s;
     int a,b;
     int i;
-    s = 0;
-    for(i = 1; i <= 100; i++){
-        s += 1/(float)i;
+    s = 1;
+    a = 1;
+    b = 1;
+    for(i = 0; i < 19; i++){
+        a += 2;
+        b *= 2;
+        s += ((float)a)/(b);
     }
-
     printf("%.2f\n",s);
 
     return 0;
