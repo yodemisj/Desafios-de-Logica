744bd906218d42dca414b2fd961ec51d906f6026
yodemisj <yodemisj@gmail.com>
2023-01-17 19:58:09 -0300

    Resolução do exercício Sequencias S

---
+++ Iniciante/C/SequenciaS.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    float s;
+    int i;
+    s = 0;
+    for(i = 1; i <= 100; i++){
+        s += 1/(float)i;
+    }
+
+    printf("%.2f\n",s);
+
+    return 0;
+}
