c7898303db8b5d458db361bfea51b776544dd76c
yodemisj <yodemisj@gmail.com>
2023-01-18 16:33:13 -0300

    Resolução do Exercício Preenchimento de Vetor I

---
+++ Iniciante/C/PreenchimentoDeVetorI.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    int x[10];
+    int i;
+
+    scanf("%d",&x[0]);
+
+    for(i = 1; i < 10; i++){
+        x[i] = x[i-1] * 2;
+    }
+
+    for(i = 0; i < 10; i++){
+        printf("N[%d] = %d\n",i,x[i]);
+    }
+
+
+    return 0;
+}
