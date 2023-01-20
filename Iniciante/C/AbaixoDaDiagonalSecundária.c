78b8221e1cc75546696ad06d80a991564f8b17e3
yodemisj <yodemisj@gmail.com>
2023-01-19 23:37:41 -0300

    Resolução do exercício Abaixo da Diagonal Secundária

---
+++ Iniciante/C/AbaixoDaDiagonalSecundária.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+    int i,j;
+    float M[12][12], resultado = 0;
+    int fimC = 11;
+    char operacao;
+    
+    scanf(" %c",&operacao);
+    for(i = 0; i < 12; i++){
+        for(j = 0; j < 12; j++){
+            scanf("%f",&M[i][j]);
+        }
+    }
+
+    for(i = 1; i < 12; i++){
+        for(j = 11; j >= fimC; j--){
+            resultado += M[i][j];
+        }
+        fimC--;
+    }
+
+    if(operacao == 'M'){
+        resultado = resultado/66; 
+    }
+
+    printf("%.1f\n",resultado);
+
+    return 0;
+}
