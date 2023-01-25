70e27676c8ab9448bf7d736760ae31d3a36f8897
yodemisj <yodemisj@gmail.com>
2023-01-24 21:44:59 -0300

    Resolução do exercício Tomadas

---
+++ Iniciante/C/Tomadas.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int t1,t2,t3,t4;
+    int soma = 0;
+    scanf("%d %d %d %d",&t1,&t2,&t3,&t4);
+    soma = t1 + t2 + t3 + t4 - 3;
+    printf("%d\n",soma);
+
+    return 0;
+}
