36007fc10a492d5549cea31d729f0810de9e268e
yodemisj <yodemisj@gmail.com>
2023-01-29 20:41:41 -0300

    Resolução do exercício Polígonos Regulares Simples

---
+++ Iniciante/C/PoligonosRegulares.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    long long int resposta;
+    long long int c,l;
+
+    scanf("%lld",&l);
+    scanf("%lld",&c);
+    resposta = l*c;
+    printf("%lld\n",resposta);
+
+    return 0;
+}
