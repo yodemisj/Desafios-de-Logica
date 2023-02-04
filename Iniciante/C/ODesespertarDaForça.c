47259e3e00c06146d02756d62ea5115c76454e8e
yodemisj <yodemisj@gmail.com>
2023-02-04 00:23:06 -0300

    Resolução do exercício O Despertar da Força

--- Iniciante/C/ODesespertarDaForça.c
+++ Iniciante/C/ODesespertarDaForça.c
@@ -2,8 +2,7 @@
 
 int main() {
     int achou = 0;
-    int n,m,i,j,k,l;
-    int x,y;
+    int n,m,i,j;
     int matriz[1000][1000];
 
     scanf("%d %d",&n,&m);
@@ -16,32 +15,19 @@ int main() {
 
     for(i=1; i<n-1; i++){
         for(j=1; j<m-1; j++){
-            if(matriz[i][j] == 42){
+            if(matriz[i][j] == 42 && matriz[i][j-1] == 7 && matriz[i][j+1] == 7 &&
+            matriz[i-1][j] == 7 && matriz[i-1][j-1] == 7 && matriz[i-1][j+1] == 7 &&
+            matriz[i+1][j] == 7 && matriz[i+1][j-1] == 7 && matriz[i+1][j+1] == 7){
+                    printf("%d %d\n",i+1,j+1);
                     achou = 1;
-                    for(k = i-1; k<(i+2); k++){
-                        for(l=j-1; l<(j+2);l++){
-                            if(k != i && l != j){
-                                if(matriz[k][l] != 7){
-                                    achou = 0;
-                                    break;
-                                }
-                            }
-                        }
-                    if(achou != 0){
-                        x = i + 1;
-                        y = j + 1;
-                        break;
-                    }else{
-                        x = 0;
-                        j = 0;
-                    }
+                    break;
                 }
             }
+            if(achou)break;
         }
-        if(achou)break;
+    if(achou == 0){
+        printf("0 0\n");
     }
-
-    printf("%d %d\n",x,y);
 
 
     return 0;
