b5ec456315a3e961cd18dd446c86579376894d9a
yodemisj <yodemisj@gmail.com>
2023-02-09 23:00:45 -0300

    Resolução do exercício feliznataal!

---
+++ Iniciante/C/feliznataaal!.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int qtd, i;
+
+    scanf("%d",&qtd);
+
+    printf("Feliz nat");
+    for(i=0; i<qtd; i++){
+        printf("a");
+    }
+    printf("l!\n");
+    return 0;
+}
