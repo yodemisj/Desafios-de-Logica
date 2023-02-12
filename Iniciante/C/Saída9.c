b9383956b4cc76027c7ac18208ef919329d27220
yodemisj <yodemisj@gmail.com>
2023-02-12 18:54:46 -0300

    Resolução do exercício Saída 9

---
+++ Iniciante/C/Saída9.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    
+    printf("\"Ro\'b\'er\t\\/\"\n");
+    printf("(._.) ( l: ) ( .-. ) ( :l ) (._.)\n");
+    printf("(^_-) (-_-) (-_^)\n");
+    printf("(\"_\") (\'.\')\n");
+    
+    return 0;
+}
