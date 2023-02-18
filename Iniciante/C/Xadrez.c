e6ceedf582882d5e6b3b22831a90e4e342582c3d
yodemisj <yodemisj@gmail.com>
2023-02-18 20:44:03 -0300

    Resolução do exercício Xadrez

---
+++ Iniciante/C/Xadrez.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    scanf("%d %d",&a,&b);
+    
+    if(a%2 == 0 && b%2==0){
+        printf("1\n");
+    }else if(a%2 == 0 && b%2!=0){
+        printf("0\n");
+    }else if(a%2 != 0 && b%2==0){
+        printf("0\n");
+    }else{
+        printf("1\n");
+    }
+    return 0;
+}
