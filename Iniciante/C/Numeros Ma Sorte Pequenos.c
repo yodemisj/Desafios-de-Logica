a13b5814523207bda9e5039a9db5aa9ef9ab7bf6
yodemisj <yodemisj@gmail.com>
2023-03-28 23:56:52 -0300

    Numero Ma Sorte Pequenos

---
+++ Iniciante/C/Numeros Ma Sorte Pequenos.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char numero[20];
+    int tamanho, i,ma=0;
+
+    scanf("%s",numero);
+
+    tamanho = strlen(numero);
+
+    for(i=0; i<tamanho-2;i++){
+        if(numero[i] == '1'&& numero[i+1] == '3'){
+            ma=1;
+            break;
+        }
+    }
+
+    if(ma == 0) printf("%s es de Mala Suerte\n",numero);
+    else printf("%s NO es de Mala Suerte\n",numero);
+
+    return 0;
+    
+}
