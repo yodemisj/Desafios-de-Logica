925d8a754d57ffd70c52343c8ddee7299159c4f6
yodemisj <yodemisj@gmail.com>
2023-03-29 21:42:40 -0300

    Resolução do exercício Ajude um Candidaro a PhD!

---
+++ Iniciante/C/Ajude um Candidato a PhD!.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int N,i,j;
+    char operador,lixo[10];
+    int a,b;
+    scanf("%d",&N);
+
+    for(i=0; i<N; i++){
+        scanf(" %d",&a);
+        scanf(" %c",&operador);
+        scanf(" %d",&b);
+
+        if (operador != '+'){
+            scanf(" %s",lixo);
+            printf("skipped\n");
+        }else{
+            a = a + b;
+            printf("%d\n",a);
+        }
+        
+    }
+
+
+    return 0;
+}
